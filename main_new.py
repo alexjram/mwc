2f968c26bdf89cc38b77e30651702ba7ed454bfa
Alexis Ramírez <alexis.jrm.0395@gmail.com>
2024-09-19 15:36:12 -0400

    start adding methods to process the json to something better

--- main_new.py
+++ main_new.py
@@ -1,5 +1,7 @@
+import json
 from time import sleep
 import os
+from typing import Union
 from dotenv import load_dotenv
 
 from utils.backend_client import BackendClient
@@ -12,6 +14,7 @@ class Main:
     enabled: bool
     secondToRetry: int
     client: BackendClient
+    data: list[dict[str, Union[str, list[dict]]]]
     
     def start(self) -> None:
         
@@ -23,6 +26,20 @@ class Main:
 
         self.enabled = True
         self.secondToRetry = 30
+        
+        file = open(os.getenv('DATA_FILE') or '', 'r')
+        data = json.load(file)
+        file.close()
+        
+        self.client.check_if_server_is_up()
+        
+        
+        
         while self.enabled:
             print('2 seconds passed')
             sleep(2)
+            
+    def normalize_data(self, data: list):
+        
+        for datum in data:
+            pass
